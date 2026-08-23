db5330c08b1969831f01ad42b78ce154a02e390a
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-24 00:01:14 +0200

    create production line models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,22 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Inambu_Manufacturing_Pty.Models;
 
 namespace Inambu_Manufacturing_Pty.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+  public DbSet<ProductionLine> ProductionLines { get; set; } // registering the productionLine model
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+
+    // HasData populates the tables with 3 production lines
+    builder.Entity<ProductionLine>().HasData( // targets the production table
+      new ProductionLine { Id = 1, Name = "Production Line 1" },
+      new ProductionLine { Id = 2, Name = "Production Line 2" },
+      new ProductionLine { Id = 3, Name = "Production Line 3" }
+    );
+  }
 }
---
+++ Models/ProductionLine.cs
@@ -0,0 +1,7 @@
+namespace Inambu_Manufacturing_Pty.Models;
+
+public class ProductionLine
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = string.Empty;
+}
