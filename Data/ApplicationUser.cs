50c06df288b1057e0c3327a632581b75d7ee8889
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-26 17:27:13 +0200

    add the three new users to the database

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace Inambu_Manufacturing_Pty.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+  public string DisplayName  { get; set; } = string.Empty;
+  public StaffLevel StaffLevel { get; set; } = StaffLevel.User;
 }
 
